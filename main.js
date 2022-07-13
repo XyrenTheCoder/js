134e61be3107081a02cd613ab2c6d64e16c25735
Kamilla Youver <95626487+archisha69@users.noreply.github.com>
2022-07-13 13:07:44 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
 const Discord = require("discord.js")
 const client = new Discord.Client()
+const axios = require('axios')
 
 client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`)
+    console.log('>> Logged in as ${client.user.tag}')
 })
 
-client.on("message", msg => {
-  if (msg.content === "!ping") {
-    msg.reply("Client Latency: ${round(client.latency * 1000)}ms");
-  }
+client.on('message', async msg => {
+    async function getMeme(){
+        const res = await axios.get('https://memeapi.pythonanywhere.com/');
+        return res.data.memes[0].url;
+    }
+    switch (msg.content) {
+        case "$ping":
+            msg.reply("Client Latency: ${round(client.latency * 1000)}ms");
+            break;
+        case "!meme":
+//          msg.channel.send("");
+            const img = await getMeme();
+            msg.channel.send(img);
+            break;
+    }
 })
 
 client.login(process.env.TOKEN)
